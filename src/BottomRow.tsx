aaaf42aa4e31d463c96c76b9a44bfe0205e99c34
Mia Chen <mia.chen.mtl@gmail.com>
2022-04-28 22:07:12 -0400

    Fix BottomRow comonent and switch prop to revealedTotalScore

--- src/BottomRow.tsx
+++ src/BottomRow.tsx
@@ -1,16 +1,21 @@
 interface BottomRowProps {
   wordLength: number;
-  totalScore: number;
+  revealedTotalScore: string;
 }
 
 export function BottomRow(props: BottomRowProps) {
-  const { wordLength, totalScore } = props;
+  const { wordLength, revealedTotalScore } = props;
   const cells = Array(wordLength)
     .fill(0)
     .map((_, i) => <td key={i} className="Row-letter hidden" />);
+  cells.push(
+    <td key={wordLength} className="Row-Score">
+      {revealedTotalScore}
+    </td>
+  );
 
   return (
-    <tr key="bottom" className="Bottom-Row" data-row-score={totalScore}>
+    <tr key="bottom" className="Bottom-Row">
       {cells}
     </tr>
   );
